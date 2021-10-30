9a5df0871b7c33631f80a7b2fda4687db455e0b2
davesahaj <sahajdavework@pm.me>
2021-10-30 17:41:05 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+import Join from "./components/Join/Join";
+function App() {
+  return (
+    <div className="App">
+      <Join />
+    </div>
+  );
+}
+
+export default App;
