ecc9205af6bf7c901e7a08f5751ed9510ce8b0ac
davesahaj <sahajdavework@pm.me>
2021-11-08 17:41:05 +0530

    initial commit

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,44 +1,27 @@
 import "./Join.scss";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
-const Join = () => {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("");
-
+function Join() {
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div>
-          <h1 className="heading">Join</h1>
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Name"
-            className="joinInput"
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Room ID"
-            className="joinInput"
-            onChange={(event) => setRoom(event.target.value)}
-          />
-        </div>
-        <Link
-          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-          <button className="button" type="submit">
-            Sign In
-          </button>
-        </Link>
+    <div className="join-wrapper">
+      <div className="join-title">
+        <span className="cine"> Cine</span>
+        <span className="nights"> Nights.</span>
+      </div>
+      <div className="join-form">
+        <span>Welcome</span>
+
+        <input
+          type="text"
+          name="username"
+          placeholder="User Name"
+          id="username"
+        />
+
+        <input type="text" name="room" placeholder="Room ID" id="room" />
+
+        <button type="submit">Enter</button>
       </div>
     </div>
   );
-};
+}
 
 export default Join;
