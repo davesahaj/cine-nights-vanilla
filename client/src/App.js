1a132464e726466753c37162319e6d3bd9d366a0
davesahaj <sahajdavework@pm.me>
2022-01-16 20:44:18 +0530

    Added: New react app with styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import "./App.scss";
-import Home from "./components/Home/Home";
-import VideoScreen from "./components/Video/VideoScreen";
-import { socket, socketContext } from "./socket";
-import { Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <socketContext.Provider value={socket}>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/room/:roomid" element={<VideoScreen />} />
-        </Routes>
-      </socketContext.Provider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
