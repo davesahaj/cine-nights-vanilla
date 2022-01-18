7d4d4798fbb8b55af6852d37e8fa7d0ea67c7ba5
davesahaj <sahajdavework@pm.me>
2022-01-18 14:20:19 +0530

    Added: Axios backend request and video playback

--- client/src/components/CreateRoom.jsx
+++ client/src/components/CreateRoom.jsx
@@ -1,14 +1,33 @@
 import { CreateRoomContainer } from "./styles/CreateRoom.styled";
 import { useContext, useRef } from "react";
 import { socketContext } from "../contexts/socketContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const CreateRoom = () => {
+  const navigate = useNavigate();
+  const baseURL = `http://localhost:4000`;
   const roomRef = useRef(null);
   const fileRef = useRef(null);
   const user = useContext(socketContext);
 
   const createRoom = () => {
-    //TODO: Upload file to backend
+    axios
+      .post(baseURL + "/create", {
+        username: user.user,
+        userid: user.socket.id,
+      })
+      .then((res) => {
+        user.updateRoom(res.data.room);
+        // TODO: make an async code to user user.room instead of
+        // res.data.room in navigate statement
+        // right now user.room is null
+        navigate(`/room/${res.data.room}`, { replace: true });
+      })
+      .catch((err) => {
+        console.log(err);
+        //TODO: SHOW ERROR IN ERROR COMPONENT
+      });
   };
   return (
     <CreateRoomContainer>
--- client/src/components/VideoScreen.jsx
+++ client/src/components/VideoScreen.jsx
@@ -1,7 +1,71 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { socketContext } from "../contexts/socketContext";
 import { VideoScreenContainer } from "./styles/VideoScreen.styled";
 
+const ChatMessage = ({ message }) => {
+  return (
+    <div className="chat-message">
+      <div className="name-div">{message.userName}:</div>
+      <div className="message-div">{message.messageText}</div>
+    </div>
+  );
+};
+
 const VideoScreen = () => {
-  return <VideoScreenContainer>room created</VideoScreenContainer>;
+  const messageRef = React.createRef();
+  let params = useParams();
+  const user = useContext(socketContext);
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    if (!user.socket) return;
+    user.socket.emit("joinRoom", { username: user.user, room: params.roomid });
+    user.socket.on(`messageFromServer`, (message) => {
+      setMessageList((currentMessages) => [...currentMessages, message]);
+    });
+
+    // socket.on("error", (message) => {
+    //TODO: add a floating error popup
+    // });
+  }, []);
+
+  function sendMessage() {
+    const textToSend = messageRef.current.value;
+    if (textToSend) {
+      user.socket.emit(
+        `messageFromClient`,
+        `${textToSend}`,
+        function callback(res) {
+          if (!res.delivered) {
+            console.log("error");
+          }
+          // TODO: add error if message is not delivered
+        }
+      );
+    }
+  }
+
+  return (
+    <VideoScreenContainer>
+      <div className="video-screen">
+        <video id="videoPlayer" controls muted="muted" autoPlay>
+          <source src="/file.webm" type="video/mp4" />
+        </video>
+      </div>
+      <div className="chat-box">
+        <div className="chat-window">
+          {messageList.map((message, index) => (
+            <ChatMessage message={message} key={index} />
+          ))}
+        </div>
+        <div className="chat-textarea">
+          <input type="text" ref={messageRef} />
+          <button onClick={sendMessage}>send</button>
+        </div>
+      </div>
+    </VideoScreenContainer>
+  );
 };
 
 export default VideoScreen;
