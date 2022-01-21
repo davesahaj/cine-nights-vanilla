6c448be29d97c855193e421a4ef84dca5a9c4b7d
davesahaj <sahajdavework@pm.me>
2022-01-21 17:23:47 +0530

    Added: Global Theme. Removed: User Check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,23 @@
-import { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import VideoScreen from "./components/VideoScreen";
 import PageNotFound from "./components/PageNotFound";
 import CreateRoom from "./components/CreateRoom";
 import Header from "./components/Header.jsx";
-import UserModal from "./components/UserModal";
-import { socketContext } from "./contexts/socketContext";
+import { dark } from "./theme";
+import { ThemeProvider } from "styled-components";
 
 function App() {
-  const user = useContext(socketContext);
-
-  function checkUser(component) {
-    return !user.user ? <UserModal /> : component;
-  }
-
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/create" element={checkUser(<CreateRoom />)} />
-        <Route exact path="/room/" element={checkUser(<VideoScreen />)} />
-        <Route
-          exact
-          path="/room/:roomid"
-          element={checkUser(<VideoScreen />)}
-        />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <ThemeProvider theme={dark}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/room/new" element={<CreateRoom />} />
+          <Route exact path="/room/:roomid" element={<VideoScreen />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
